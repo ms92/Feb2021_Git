c7ec78afe965200376112ac84208e20ba84698fc
ms92 <mamatas2021@gmail.com>
2021-07-23 18:25:02 -0700

    3rd commit

---
+++ src/main/java/variousConcepts/GitTest2.java
@@ -0,0 +1,12 @@
+package variousConcepts;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+	System.out.println("Git Test2");
+	System.out.println("Git Test1");
+	System.out.println("Git Test");
+
+	}
+
+}
